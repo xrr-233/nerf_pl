61e73a60c8216da414e44b912c695fe6ba61a6bf
xrr-233 <1846399636@qq.com>
2023-03-17 14:33:09 +0800

    upd

--- neus2llff.py
+++ neus2llff.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import shutil
 
 import cv2
 import numpy as np
@@ -60,18 +61,19 @@ W = sample_img.width
 H = sample_img.height
 
 os.makedirs('neus2llff_preprocessed', exist_ok=True)
-root_dir = os.path.join('neus2llff_preprocessed', args.exp_name)
-os.makedirs(root_dir, exist_ok=True)
-os.makedirs(os.path.join(root_dir, 'sparse'), exist_ok=True)
-os.makedirs(os.path.join(root_dir, 'sparse', '0'), exist_ok=True)
-root_dir = os.path.join(root_dir, 'sparse', '0')
+new_dir = os.path.join('neus2llff_preprocessed', args.exp_name)
+os.makedirs(new_dir, exist_ok=True)
+shutil.copytree(os.path.join(root_dir, 'image'), os.path.join(new_dir, 'images'))
+os.makedirs(os.path.join(new_dir, 'sparse'), exist_ok=True)
+os.makedirs(os.path.join(new_dir, 'sparse', '0'), exist_ok=True)
+new_dir = os.path.join(new_dir, 'sparse', '0')
 
-with open(os.path.join(root_dir, 'cameras.txt'), 'w') as f:
+with open(os.path.join(new_dir, 'cameras.txt'), 'w') as f:
     # print(camera_intrinsics[0])
     for i in range(n_images):
         f.write(f'{i + 1} SIMPLE_PINHOLE {W} {H} {camera_intrinsics[i][0, 0]} {W // 2} {H // 2}\n')
 
-with open(os.path.join(root_dir, 'images.txt'), 'w') as f:
+with open(os.path.join(new_dir, 'images.txt'), 'w') as f:
     print(camera_extrinsics[0])
     # print(qvec2rotmat([0.83954509, 0.05650705, -0.43843359, -0.31582745]))
     for i in range(n_images):
@@ -81,5 +83,5 @@ with open(os.path.join(root_dir, 'images.txt'), 'w') as f:
         tvec = c2w_mat[:3, 3].flat
         f.write(f'{i + 1} {qvec[0]} {qvec[1]} {qvec[2]} {qvec[3]} {tvec[0]} {tvec[1]} {tvec[2]} {i + 1} {img_dir[0]}\n\n')
 
-with open(os.path.join(root_dir, 'points3D.txt'), 'w') as f:
+with open(os.path.join(new_dir, 'points3D.txt'), 'w') as f:
     pass
