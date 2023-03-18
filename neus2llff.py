3524576bd285c25478cf96a7576e53029b5470fc
xrr-233 <1846399636@qq.com>
2023-03-19 00:20:53 +0800

    upd

--- neus2llff.py
+++ neus2llff.py
@@ -1,12 +1,11 @@
 import argparse
 import os
 import shutil
-
 import cv2
 import numpy as np
 from PIL import Image
-
-from datasets.colmap_utils import rotmat2qvec, qvec2rotmat
+from datasets.colmap_wrapper import run_colmap
+from datasets.colmap_utils import rotmat2qvec
 
 
 def load_K_Rt_from_P(filename, P=None):
@@ -34,12 +33,12 @@ def load_K_Rt_from_P(filename, P=None):
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--root_dir', type=str,
-                    default='./BlendedMVS_preprocessed/5a7d3db14989e929563eb153',
                     help='identify neus preprocesses root dir')
 parser.add_argument('--exp_name', type=str, default='exp',
                     help='experiment name')
 args = parser.parse_args()
 root_dir = args.root_dir
+exp_name = args.exp_name
 
 camera_dict = np.load(os.path.join(root_dir, 'cameras_sphere.npz'))
 n_images = len(camera_dict) // 6
@@ -61,21 +60,21 @@ W = sample_img.width
 H = sample_img.height
 
 os.makedirs('neus2llff_preprocessed', exist_ok=True)
-new_dir = os.path.join('neus2llff_preprocessed', args.exp_name)
+new_dir = os.path.join(os.getcwd(), 'neus2llff_preprocessed', exp_name)
 os.makedirs(new_dir, exist_ok=True)
 if os.path.exists(os.path.join(new_dir, 'images')):
     shutil.rmtree(os.path.join(new_dir, 'images'))
 shutil.copytree(os.path.join(root_dir, 'image'), os.path.join(new_dir, 'images'))
 os.makedirs(os.path.join(new_dir, 'sparse'), exist_ok=True)
 os.makedirs(os.path.join(new_dir, 'sparse', '0_raw'), exist_ok=True)
-new_dir = os.path.join(new_dir, 'sparse', '0_raw')
+os.makedirs(os.path.join(new_dir, 'sparse', '0'), exist_ok=True)
 
-with open(os.path.join(new_dir, 'cameras.txt'), 'w') as f:
+with open(os.path.join(new_dir, 'sparse', '0_raw', 'cameras.txt'), 'w') as f:
     # print(camera_intrinsics[0])
     for i in range(n_images):
         f.write(f'{i + 1} SIMPLE_PINHOLE {W} {H} {camera_intrinsics[i][0, 0]} {W // 2} {H // 2}\n')
 
-with open(os.path.join(new_dir, 'images.txt'), 'w') as f:
+with open(os.path.join(new_dir, 'sparse', '0_raw', 'images.txt'), 'w') as f:
     # print(qvec2rotmat([0.83954509, 0.05650705, -0.43843359, -0.31582745]))
     for i in range(n_images):
         w2c_mat = camera_extrinsics[i]
@@ -84,5 +83,9 @@ with open(os.path.join(new_dir, 'images.txt'), 'w') as f:
         tvec = c2w_mat[:3, 3].flat
         f.write(f'{i + 1} {qvec[0]} {qvec[1]} {qvec[2]} {qvec[3]} {tvec[0]} {tvec[1]} {tvec[2]} {i + 1} {img_dir[i]}\n\n')
 
-with open(os.path.join(new_dir, 'points3D.txt'), 'w') as f:
+with open(os.path.join(new_dir, 'sparse', '0_raw', 'points3D.txt'), 'w') as f:
     pass
+
+run_colmap(new_dir)
+
+shutil.rmtree(os.path.join(new_dir, 'sparse', '0_raw'))
